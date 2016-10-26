2d321bdea8938789bfd200cc201bd5991d3f9acb
Thomas Narro <Thomas.Narro@gmail.com>
2016-10-26 05:27:50 -0400

    added views, css and bootstrap framework for lecture 7

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,65 +8,85 @@ use App\Http\Requests;
 
 class BookController extends Controller
 {
+
     /**
-     * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return 'Hi, This is from the book controller!';//
+        return 'Hi, This is from the book controller. TO DO: Display a listing of all the books';//
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Get
      */
     public function create()
     {
-        //
+        return view('book.create');
     }
 
     /**
-     * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+
+        # Validate
+        $this->validate($request, [
+            'title' => 'required|min:3',
+        ]);
+
+        # If there were errors, Laravel will redirect the
+        # user back to the page that submitted this request
+        # The validator will tack on the form data to the request
+        # so that it's possible (but not required) to pre-fill the
+        # form fields with the data the user had entered
+
+        # If there were NO errors, the script will continue...
+
+        # Get the data from the form
+        #$title = $_POST['title']; # Option 1) Old way, don't do this.
+        $title = $request->input('title'); # Option 2) USE THIS ONE! :)
+
+        # Here's where your code for what happens next should go.
+        # Examples:
+        # Save book in the database
+
+        # When done - what should happen?
+
+        # Beginner students - you should just return a view with a confirmation page; it's the easiest option.
+        return view('book.store')->with(['title' => $title]);
+
+
+        # More advanced students, there is the option of redirecting the user back to the page
+        # they were on and display the message there.
+        # If you need to send data with this redirect please refer to:
+        # https://laravel.com/docs/5.3/redirects#redirecting-with-flashed-session-data
+
+        # return redirect('/books/create');
+
     }
 
+
     /**
-     * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function show($title)
     {
         return view('book.show')->with('title', $title);
     }
 
+
     /**
-     * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-        //
+        return 'To do: Show form to edit a book';
     }
 
     /**
-     * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
@@ -74,13 +94,33 @@ class BookController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
+    }
+
+
+    /**
+	* This was example code I wrote in Lecture 7
+    * It shows, roughly, what a controller action for your P3 might look like
+    * It is not at all related to the Book resource.
+	*/
+    public function getLoremIpsumText(Request $request)
+    {
+
+        # Validate the request....
+
+        # Generate the lorem ipsum text
+        $howManyParagraphs = $request->input('howManyParagraphs');
+
+        # Logic...
+        $loremenator = \SBuck\Loremenator();
+        $text = $loremenator->getParagraphs($howManyParagraphs);
+
+        # Display the results...
+        return view('lorem')->with(['text', $text]);
+
     }
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,6 +7,13 @@
     </title>
 
     <meta charset='utf-8'>
+
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' rel='stylesheet'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/lumen/bootstrap.min.css' rel='stylesheet'>
+
     <link href="/css/foobooks.css" type='text/css' rel='stylesheet'>
 
     {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
@@ -16,11 +23,22 @@
 <body>
 
     <header>
+        <a href='/'>
         <img
         src='http://making-the-internet.s3.amazonaws.com/laravel-foobooks-logo@2x.png'
-        style='width:300px'
-        alt='Foobooks Logo'>
+        alt='Foobooks Logo'
+        width='300px'
+        class='logo'>
+        </a>
     </header>
+
+    <nav>
+        <ul>
+            <li><a href='/books'>View all books</a></li>
+            <li><a href='/books/create'>Add a new book</a></li>
+        </ul>
+    </nav>
+
 
     <section>
         {{-- Main page content will be yielded here --}}
@@ -28,10 +46,10 @@
     </section>
 
     <footer>
-        &copy; {{ date('Y') }}
-    </footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+     &copy; {{ date('Y') }} &nbsp;&nbsp;
+     <a href='https://github.com/magnet-tn/foobooks' target='_blank'><i class='fa fa-github'></i> View on Github</a> &nbsp;&nbsp;
+     <a href='http://foobooks.troubleu.com' target='_blank'><i class='fa fa-link'></i> View Live</a>
+ </footer>
 
     {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
     @yield('body')
